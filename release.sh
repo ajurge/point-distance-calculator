e1362e988d04d71f4d6ecbc354833fbcb95caa16
ajurge <ajurge@gmail.com>
2018-04-02 23:36:13 +0200

    add release.sh and update travis ci configuration; add bash script to publish released versions to Maven Central

---
+++ release.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+RELEASE_VERSION=$1
+DEVELOPMENT_VERSION=$2
+VERSION_VALIDATION_REGEX='^([0-9]+\.){2,2}(\*|[0-9]+)$'
+
+if [ -z "$RELEASE_VERSION" ]
+  then
+    echo "ERROR: No release version supplied"
+    exit 1
+fi
+
+if [ -z "$DEVELOPMENT_VERSION" ]
+  then
+    echo "ERROR: No development version supplied"
+    exit 1
+fi
+
+validate_version () {
+  VERSION=$1
+  if [[ ! $VERSION =~ $VERSION_VALIDATION_REGEX ]]; then
+    echo "ERROR: Invalid version '$VERSION'"
+    echo "INFO: Valid version format MAJOR.MINOR.PATCH see https://semver.org/"
+    exit 1
+  fi
+}
+
+validate_version $RELEASE_VERSION
+validate_version $DEVELOPMENT_VERSION
+
+git checkout master &&
+git pull &&
+
+if [ $(git tag -l "point-distance-calculator-$RELEASE_VERSION") ]
+    then
+        echo "Version $RELEASE_VERSION already exists. Release aborted!"
+        exit 1
+fi
+
+./mvnw --settings settings.xml -B release:clean release:prepare release:perform -DreleaseVersion=$RELEASE_VERSION -DdevelopmentVersion=$DEVELOPMENT_VERSION-SNAPSHOT &&
+. ./sync-version-artifacts-to-maven-central.sh
